79110e17cf45015fe32a0ed06fa530838d31a0ba
FranchiniMae <fmae.mercado@gmail.com>
2016-04-03 01:01:38 -0700

    Adds working home.html without the separated templates

---
+++ paceyapp/public/javascripts/angularApp.js
@@ -0,0 +1,74 @@
+var app = angular.module('paceyApp', ['ui.router']);
+
+app.factory('goals', [function() {
+	var o = {
+		goals: []
+	};
+	return o;
+}]);
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	'$locationProvider',
+	function ($stateProvider, $urlRouterProvider, $locationProvider){
+
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+			})
+			.state('goals', {
+				url: '/goals/{id}',
+				templateUrl: '/goals.html',
+				controller: 'GoalsCtrl'
+			});
+
+		$urlRouterProvider.otherwise('home');
+
+	  // $locationProvider.html5Mode({
+   //    enabled: true,
+   //    requireBase: false
+   //  });
+}]);
+
+app.controller('MainCtrl', [
+'$scope',
+'goals',
+function($scope, goals){
+	$scope.goals = goals.goals;
+	
+  $scope.addGoal = function() {
+  	if(!$scope.title || $scope.title === '') { return; }
+  	$scope.goals.push({
+  		title: $scope.title,
+  		goalDate: $scope.goalDate,
+  		tasks: [
+  			{description: "first task"},
+  			{description: "second task"}
+  		]
+  	});
+  	$scope.title = '';
+  	$scope.goalDate = '';
+  };
+}]);
+
+app.controller('GoalsCtrl', [
+	'$scope',
+	'$stateParams',
+	'goals',
+	function($scope, $stateParams, goals){
+		$scope.goal = goals.goals[$stateParams.id];
+
+		$scope.addTask = function() {
+			if ($scope.description === '') { return; }
+			$scope.goal.tasks.push({
+				description: $scope.description
+			});
+			$scope.description = '';
+		};
+	}
+]);
+
+
