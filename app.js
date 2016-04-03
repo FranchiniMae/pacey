bff1aee685f19711478557ba4ddb7c7b28781bba
FranchiniMae <fmae.mercado@gmail.com>
2016-04-02 19:15:29 -0700

    Adds post function

---
+++ app.js
@@ -0,0 +1,19 @@
+var app = angular.module('paceyApp', []);
+
+app.controller('MainCtrl', [
+'$scope',
+function($scope){
+  $scope.test = 'Hello world!';
+  $scope.goals = [
+  	{title: 'goal 1'},
+  	{title: 'goal 2'},
+  	{title: 'goal 3'},
+  	{title: 'goal 4'},
+  	{title: 'goal 5'}
+  ];
+  $scope.addGoal = function() {
+  	if(!$scope.title || $scope.title === '') { return; }
+  	$scope.goals.push({title: $scope.title});
+  	$scope.title = '';
+  };
+}]);
